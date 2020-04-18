d25a1f902efce0f8ab13c1b8647c17ee7751ef1d
Cheng-Yu Pai <mail4charliepai@gmail.com>
2020-04-18 03:21:12 -0700

    Implement registration

---
+++ app/main.py
@@ -0,0 +1,36 @@
+from cachetools import TTLCache
+from fastapi import Cookie, FastAPI, Form, Request, Response
+from starlette.responses import RedirectResponse
+from starlette.templating import Jinja2Templates
+
+app = FastAPI()
+
+sessions = TTLCache(100, 3600)
+
+templates = Jinja2Templates(directory="templates")
+
+
+@app.get("/")
+async def root(request: Request, name: str = Cookie(None), team: int = Cookie(None)):
+    data = {
+        "registered": name is not None and team is not None,
+        "name": name,
+        "team": team
+    }
+    return templates.TemplateResponse("index.html", {"request": request, "data": data})
+
+
+@app.get("/register")
+async def register(name: str, team: int):
+    response = RedirectResponse(url="/")
+    response.set_cookie(key="name", value=name)
+    response.set_cookie(key="team", value=team)
+    return response
+
+
+@app.get("/unregister")
+async def unregister():
+    response = RedirectResponse(url="/")
+    response.delete_cookie(key="name")
+    response.delete_cookie(key="team")
+    return response
