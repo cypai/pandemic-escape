42362772fdebaaaa124bf2750ac1a1e53a42a5d1
Cheng-Yu Pai <mail4charliepai@gmail.com>
2020-04-18 21:45:04 -0700

    Implement cyan room

--- app/main.py
+++ app/main.py
@@ -1,3 +1,6 @@
+import string
+import random
+
 from cachetools import TTLCache
 from fastapi import Cookie, Depends, FastAPI, Form, Request, Response
 from fastapi.staticfiles import StaticFiles
@@ -8,6 +11,9 @@ app = FastAPI()
 app.mount("/static", StaticFiles(directory="static"), name="static")
 
 team_progress = TTLCache(100, 3600)
+room4_code = TTLCache(100, 3600)
+room4_amount = TTLCache(100, 3600)
+room4_last_clicked = TTLCache(100, 3600)
 
 templates = Jinja2Templates(directory="templates")
 
@@ -163,13 +169,45 @@ async def room4(
         request: Request,
         unlock_failure: bool = False,
         lockbox_failed: bool = False,
+        start_btn_clicked: str = None,
+        coordinates: str = None,
         registry: Registry = Depends(require_registry)):
 
     if (registry.team, 4) in team_progress:
+        message = None
+        message_color = "color:black"
+        show_start = False
+        if start_btn_clicked:
+            if registry.team not in room4_amount:
+                clear_room4_state(registry)
+                message = next_room4_state(registry)
+        else:
+            if registry.team in room4_amount:
+                if coordinates is None:
+                    pass
+                elif registry.name == room4_last_clicked[registry.team]:
+                    message = "Error! Assistance is required."
+                    message_color = "color:red"
+                    show_start = True
+                    clear_room4_state(registry)
+                elif coordinates != room4_code[registry.team]:
+                    message = "Error! Incorrect."
+                    message_color = "color:red"
+                    show_start = True
+                    clear_room4_state(registry)
+                else:
+                    message = next_room4_state(registry)
+            else:
+                # First time in room
+                show_start = True
         return templates.TemplateResponse("room4.html",
                 {
                     "request": request,
                     "lockbox_failed": lockbox_failed,
+                    "chars": string.ascii_uppercase[:10],
+                    "show_start": show_start,
+                    "message": message,
+                    "message_color": message_color,
                 })
     else:
         return locked_room_template(request,
@@ -178,6 +216,29 @@ async def room4(
                 "color:cyan",
                 4,
                 unlock_failure)
+
+
+def next_room4_state(registry: Registry):
+    room4_last_clicked[registry.team] = registry.name
+    if registry.team in room4_amount:
+        room4_amount[registry.team] += 1
+    else:
+        room4_amount[registry.team] = 0
+    if room4_amount[registry.team] >= 10:
+        code = "Passcode: 167324"
+    else:
+        code = random.choice(string.ascii_uppercase[:10]) + str(random.randint(1, 10))
+    room4_code[registry.team] = code
+    return code
+
+
+def clear_room4_state(registry: Registry):
+    if registry.team in room4_amount:
+        del room4_amount[registry.team]
+    if registry.team in room4_code:
+        del room4_code[registry.team]
+    if registry.team in room4_last_clicked:
+        del room4_last_clicked[registry.team]
 
 
 @app.get("/locked_room")
@@ -192,3 +253,28 @@ async def locked_room_verifier(
             return RedirectResponse(url="/room4")
         else:
             return RedirectResponse(url="/room4?unlock_failure=true")
+
+
+@app.get("/room4/lockbox")
+async def room4_lockbox(
+        request: Request,
+        key: str,
+        registry: Registry = Depends(require_registry)):
+
+    if key == "167324":
+        return RedirectResponse(url="/room4/answer167324")
+    else:
+        return RedirectResponse(url="/room4?lockbox_failed=true")
+
+
+@app.get("/room4/answer167324")
+async def room4_answer(
+        request: Request,
+        registry: Registry = Depends(require_registry)):
+
+    return answer_template(request,
+            "Room 4",
+            "Cyan Room",
+            "color:cyan",
+            'There is a note with the message: "M N"',
+            'A second note has the message: "The color key in the Red Room matches the room number."')
